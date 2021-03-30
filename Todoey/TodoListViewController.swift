b32645867f86d166fde7ce19d8213b27d1917e77
Vladislav Koshelev <VOKoshelyev@sberbank.ru>
2021-03-30 16:56:22 +0300

    add checkmark

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -34,3 +34,19 @@ extension TodoListViewController {
     }
 }
 
+// MARK: - Table View Delegate
+
+extension TodoListViewController {
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
